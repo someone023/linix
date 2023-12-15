d31ec86e56259d3d98b8a20e8317eb7491b247af
Ali Erkol <a.erkol23@tu-braunschweig.de>
2023-12-15 02:06:05 +0000

    hyprland

---
+++ home/wayland/hyprland/anyrun/default.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  inputs,
+  config,
+  self,
+  ...
+}: {
+  programs.anyrun = {
+    enable = true;
+
+    config = {
+      plugins = with inputs.anyrun.packages.${pkgs.system}; [
+        applications
+        randr
+        rink
+        shell
+        symbols
+      ];
+
+      width.fraction = 0.3;
+      y.absolute = 15;
+      hidePluginInfo = true;
+      closeOnClick = true;
+    };
+
+    extraCss = builtins.readFile "${self}/home/wayland/anyrun/theme.css";
+  };
+}
---
+++ home/wayland/hyprland/default.nix
@@ -0,0 +1,24 @@
+{
+  inputs,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./binds.nix
+    ./rules.nix
+    ./settings.nix
+  ];
+
+  home.packages = [
+    # inputs.hyprland-contrib.packages.${pkgs.system}.grimblast
+  ];
+
+  # start swayidle as part of hyprland, not sway
+  # systemd.user.services.swayidle.Install.WantedBy = lib.mkForce ["hyprland-session.target"];
+
+  # enable hyprland
+  wayland.windowManager.hyprland = {
+    enable = true;
+  };
+}
