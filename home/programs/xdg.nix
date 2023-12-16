9a124c84d9f7a4a03a4acf5f17fa6985d040dd34
Ali Erkol <a.erkol23@tu-braunschweig.de>
2023-12-16 02:14:22 +0000

    xdg

---
+++ home/programs/xdg.nix
@@ -0,0 +1,49 @@
+{ config, ... }:
+let
+  browser = [ "firefox" ];
+
+  # XDG MIME types
+  associations = builtins.mapAttrs (_: v: (map (e: "${e}.desktop") v)) {
+    "application/x-extension-htm" = browser;
+    "application/x-extension-html" = browser;
+    "application/x-extension-shtml" = browser;
+    "application/x-extension-xht" = browser;
+    "application/x-extension-xhtml" = browser;
+    "application/xhtml+xml" = browser;
+    "text/html" = browser;
+    "x-scheme-handler/about" = browser;
+    "x-scheme-handler/chrome" = [ "chromium-browser" ];
+    "x-scheme-handler/ftp" = browser;
+    "x-scheme-handler/http" = browser;
+    "x-scheme-handler/https" = browser;
+    "x-scheme-handler/unknown" = browser;
+
+    "audio/*" = [ "celluloid" ];
+    "video/*" = [ "celluloid" ];
+    "image/*" = [ "loupe" ];
+    "application/json" = browser;
+    "application/pdf" = browser;
+    "x-scheme-handler/discord" = browser;
+    "x-scheme-handler/spotify" = [ "spotify" ];
+    "x-scheme-handler/tg" = [ "telegramdesktop" ];
+  };
+in
+{
+  xdg = {
+    enable = true;
+    cacheHome = config.home.homeDirectory + "/.local/cache";
+
+    mimeApps = {
+      enable = true;
+      defaultApplications = associations;
+    };
+
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+      extraConfig = {
+        XDG_SCREENSHOTS_DIR = "${config.xdg.userDirs.pictures}/Screenshots";
+      };
+    };
+  };
+}
