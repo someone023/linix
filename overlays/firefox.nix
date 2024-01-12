273459922f1c2df5de7abf8029c8f9f005503625
Ali Erkol <a.erkol23@tu-braunschweig.de>
2024-01-12 22:25:19 +0000

    firefox

---
+++ overlays/firefox.nix
@@ -0,0 +1,24 @@
+{inputs, ...}: {
+
+  additions = final: _prev: import ../pkgs {pkgs = final;};
+
+  modifications = final: prev: {
+    nixpkgs.overlays =
+  let
+    # Change this to a rev sha to pin
+    moz-rev = "master";
+    moz-url = builtins.fetchTarball { url = "https://github.com/mozilla/nixpkgs-mozilla/archive/${moz-rev}.tar.gz";};
+    nightlyOverlay = (import "${moz-url}/firefox-overlay.nix");
+  in [
+    nightlyOverlay
+  ];
+    programs.firefox.package = pkgs.latest.firefox-nightly-bin;
+  };
+
+  unstable-packages = final: _prev: {
+    unstable = import inputs.nixpkgs-unstable {
+      system = final.system;
+      config.allowUnfree = true;
+    };
+  };
+}
