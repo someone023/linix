ccfb393ad901aaf7a544ce059c2ee47b780e0e82
Ali Erkol <a.erkol23@tu-braunschweig.de>
2024-01-16 22:58:32 +0000

    refactor

---
+++ home/gui/gtk.nix
@@ -0,0 +1,37 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Catppuccin-Mocha-Standard-Lavender-Dark";
+      package = pkgs.catppuccin-gtk.override {
+        accents = ["lavender"];
+        size = "standard";
+        #tweaks = [ "rimless" "black" ];
+        variant = "mocha";
+      };
+    };
+
+    gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+
+    iconTheme = {
+      package = pkgs.rose-pine-icon-theme-unstable;
+      name = "rose-pine";
+    };
+
+    font = {
+      name = "JetBrainsMono";
+      size = 11;
+    };
+  };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.catppuccin-cursors.mochaDark;
+    name = "Catppuccin-Mocha-Dark-Cursors";
+    size = 24;
+  };
+}
---
+++ nixos/hardware/brillo.nix
@@ -0,0 +1,1 @@
+{hardware.brillo.enable = true;}
