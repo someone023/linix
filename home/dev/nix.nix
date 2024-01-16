2fa5fed1f3a2471307eb89b239b1d0a6d7722415
Ali Erkol <a.erkol23@tu-braunschweig.de>
2024-01-16 12:58:25 +0000

    refactor

---
+++ home/dev/nix.nix
@@ -0,0 +1,14 @@
+{pkgs, ...}: {
+  home = {
+    packages = with pkgs; [
+      alejandra
+      nix-init
+      nix-tree
+      nix-update
+      nix-output-monitor
+      nixpkgs-review
+      statix
+      vimPluginsUpdater
+    ];
+  };
+}
