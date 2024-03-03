949b4acd2fe8a75d8c49202ee799cfbaddc36c10
someone023 <a.erkol@tu-braunschweig.de>
2024-03-03 19:33:05 +0100

    yep

--- host/intel.nix
+++ host/intel.nix
@@ -3,25 +3,16 @@
 
   boot = {
     kernelModules = ["kvm-intel"];
-    kernelParams = ["i915.fastboot=1"];
+    kernelParams = ["i915"];
   };
   hardware.cpu.intel.updateMicrocode = true;
   environment.systemPackages = with pkgs; [intel-gpu-tools];
+  environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";};
   hardware.opengl = {
     enable = true;
     extraPackages = with pkgs; [
       intel-compute-runtime
       intel-media-driver
-      vaapiIntel
-      vaapiVdpau
-      libvdpau-va-gl
-    ];
-
-    extraPackages32 = with pkgs.pkgsi686Linux; [
-      intel-media-driver
-      vaapiIntel
-      vaapiVdpau
-      libvdpau-va-gl
     ];
   };
 }
